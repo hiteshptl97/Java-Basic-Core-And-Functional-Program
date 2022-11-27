0b10e854c6f4aa07e9a28dd58e81eca13c610c3f
hiteshptl97 <hiteshpatil897@gmail.com>
2022-11-27 15:34:40 +0530

    check weather number even or odd

---
+++ src/basiccoreprogram/ChechWeatherNumberEvenOrOdd.java
@@ -0,0 +1,23 @@
+package basiccoreprogram;
+
+import java.util.Scanner;
+
+    public class ChechWeatherNumberEvenOrOdd {
+
+    public class EvenOrOdd {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter a number.");
+            int n = scan.nextInt();
+            scan.close();
+            checkEvenOrOdd(n);
+        }
+
+        public static void checkEvenOrOdd(int n) {
+            if (n % 2 == 0)
+                System.out.println("Number "+n+" is even.");
+            else
+                System.out.println("Number "+n+" is odd.");
+        }
+    }
+}
